93c9740be85721d3beaf69a6d4c86a0c1de971c6
RedOx98 <olaskeet@gmail.com>
2024-03-05 08:24:05 +0100

    qr generator

---
+++ prisma/migrations/20240229135005_ecobankqrcodes/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "staffIdNo" TEXT,
+    "surname" TEXT,
+    "firstName" TEXT,
+    "otherNames" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_staffIdNo_key" ON "User"("staffIdNo");
