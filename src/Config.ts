5603881ef557dfb1fd757d7c8cfa47980b5cd981
RedOx98 <olaskeet@gmail.com>
2024-08-07 10:55:16 +0100

    redirect seh

--- src/Config.ts
+++ src/Config.ts
@@ -5,12 +5,10 @@ export const config = {
     authority: 'https://login.microsoftonline.com/6400df67-1817-484e-84ae-ed3b97ca1620'
 };
 
+// For test azure environment
 // export const config = {
 //     appId: 'f99fa9e8-0986-4825-8d35-9d8ef0ec0e00',
 //     redirectUrl: 'http://localhost:3000',
 //     scopes: ['User.Read'],
 //     authority: 'https://login.microsoftonline.com/2ad72c11-ca14-4743-95d6-fbbe0f001056'
 // };
-
-// https://qr-generator-seven-red.vercel.app/
- 
