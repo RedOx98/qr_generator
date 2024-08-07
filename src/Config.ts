e71266265251fb6f669299c8c19697d8a6149950
RedOx98 <olaskeet@gmail.com>
2024-08-07 13:21:53 +0100

    update middleware

--- src/Config.ts
+++ src/Config.ts
@@ -1,14 +1,14 @@
-export const config = {
-    appId: '35fb369d-d924-4bfb-8bc9-37b59add6ff9',
-    redirectUrl: 'https://qr-generator-steel.vercel.app/',
-    scopes: ['User.Read'],
-    authority: 'https://login.microsoftonline.com/6400df67-1817-484e-84ae-ed3b97ca1620'
-};
+// export const config = {
+//     appId: '35fb369d-d924-4bfb-8bc9-37b59add6ff9',
+//     redirectUrl: 'https://qr-generator-steel.vercel.app/',
+//     scopes: ['User.Read'],
+//     authority: 'https://login.microsoftonline.com/6400df67-1817-484e-84ae-ed3b97ca1620'
+// };
 
 // For test azure environment
-// export const config = {
-//     appId: 'f99fa9e8-0986-4825-8d35-9d8ef0ec0e00',
-//     redirectUrl: 'http://localhost:3000',
-//     scopes: ['User.Read'],
-//     authority: 'https://login.microsoftonline.com/2ad72c11-ca14-4743-95d6-fbbe0f001056'
-// };
+export const config = {
+    appId: 'f99fa9e8-0986-4825-8d35-9d8ef0ec0e00',
+    redirectUrl: 'http://localhost:3000',
+    scopes: ['User.Read'],
+    authority: 'https://login.microsoftonline.com/2ad72c11-ca14-4743-95d6-fbbe0f001056'
+};
