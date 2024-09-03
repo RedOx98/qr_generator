a79936939dddd0aa8a9aee816497b9652adc3302
tosmic16 <tosinm444@gmail.com>
2024-09-03 13:18:39 +0100

    Change redirect  url

--- src/Config.ts
+++ src/Config.ts
@@ -1,6 +1,6 @@
-export const config = {
+ export const config = {
     appId: '35fb369d-d924-4bfb-8bc9-37b59add6ff9',
-    redirectUrl: 'https://qr-generator-steel.vercel.app/',
+    redirectUrl: '/',
     scopes: ['User.Read'],
     authority: 'https://login.microsoftonline.com/6400df67-1817-484e-84ae-ed3b97ca1620'
 };
@@ -8,7 +8,7 @@ export const config = {
 // For test azure environment
 // export const config = {
 //     appId: 'f99fa9e8-0986-4825-8d35-9d8ef0ec0e00',
-//     redirectUrl: 'http://localhost:3000',
+//     redirectUrl: 'http://localhost:3000/',
 //     scopes: ['User.Read'],
 //     authority: 'https://login.microsoftonline.com/2ad72c11-ca14-4743-95d6-fbbe0f001056'
 // };
