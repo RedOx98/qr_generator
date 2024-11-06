564d2e37aa3e55c10357d9da7d0ff9cb1c3224b2
tosmic16 <tosinm444@gmail.com>
2024-11-06 12:32:09 +0100

    pedigree

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,46 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "images.pexels.com",
-                port: "",
-                pathname: "/**"
-            },
-            {
-                protocol: "https",
-                hostname: "images.remotePatterns",
-                port: "",
-                pathname: "/**"
-            },
-            {
-                protocol: "https",
-                hostname: "flaticon.com",
-                port: "",
-                pathname: "/**"
-            },
-            {
-                protocol: "https",
-                hostname: "tuk-cdn.s3.amazonaws.com",
-                port: "",
-                pathname: "/**"
-            }
-        ],
-        domains: ['www.bwillcreative.com','chart.googleapis.com','images.remotePatterns']
-    },
-    
+    // experimental: {
+    //     serverActions: {
+    //         allowedOrigins: ['http://localhost:8080',],
+    //         allowedForwardedHosts: ['localhost'],
+    //     },
+    // },
 };
 
 export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: "https",
+//                 hostname: "images.pexels.com",
+//                 port: "",
+//                 pathname: "/**"
+//             },
+//             {
+//                 protocol: "https",
+//                 hostname: "images.remotePatterns",
+//                 port: "",
+//                 pathname: "/**"
+//             },
+//             {
+//                 protocol: "https",
+//                 hostname: "flaticon.com",
+//                 port: "",
+//                 pathname: "/**"
+//             },
+//             {
+//                 protocol: "https",
+//                 hostname: "tuk-cdn.s3.amazonaws.com",
+//                 port: "",
+//                 pathname: "/**"
+//             }
+//         ],
+//         domains: ['www.bwillcreative.com','chart.googleapis.com','images.remotePatterns']
+//     },
+    
+// };
+
+// export default nextConfig;
